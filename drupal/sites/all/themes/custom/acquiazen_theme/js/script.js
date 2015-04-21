29f08e3cec4db23614748fc3dfe827261ab78fff
Carl Watson <carl.watson@acquia.com>
2015-04-21 13:13:26 -0400

    added finished js to script.js

--- drupal/sites/all/themes/custom/acquiazen_theme/js/script.js
+++ drupal/sites/all/themes/custom/acquiazen_theme/js/script.js
@@ -1,41 +1,49 @@
 (function ($, Drupal, window, document, undefined) {
 
-  /**
-   * Example A
-   */
-  Drupal.behaviors.ExampleA = {
-    attach: function (context, settings) {
+    /**
+     * Example A
+     */
+    Drupal.behaviors.ExampleA = {
+        attach: function (context, settings) {
 
-      console.log('Example A loaded');
+            console.log('Example A loaded');
 
-    }
-  };
+        }
+    };
 
-  /**
-   * Hey Carl, we are using this for JS training ~
-   */
-  Drupal.behaviors.ExampleB = {
-    attach: function (context, settings) {
-      if (context === document) {
+    /**
+     * Hey Carl, we are using this for JS training ~
+     */
+    Drupal.behaviors.ExampleB = {
+        attach: function (context, settings) {
+            if (context === document) {
 
-        console.log('Example B is loaded');
-
-
-        // using variable
-        var frenchfry = $('div.apples img');
-        // grab attr
-        var frenchfryAlt = frenchfry.attr('alt');
-        console.log(frenchfryAlt);
-
-        // https://api.jquery.com/category/traversing/
-        $('.oranges .peaches').find('span').addClass('MARKERS');
-        $('.apples').closest('.oranges').addClass('anotherClass');
+                console.log('Example B is loaded');
 
 
+                // using variable
+                var frenchfry = $('div.apples img');
+                // grab attr
+                var frenchfryAlt = frenchfry.attr('alt');
+                console.log(frenchfryAlt);
 
-      }
-    }
-  };
+                // https://api.jquery.com/category/traversing/
+                $('.oranges .peaches').find('span').addClass('MARKERS');
+                $('.apples').closest('.oranges').addClass('anotherClass');
+
+                //Carl's code
+                $('.peaches').find('.nuts').addClass("border-yellow");
+                $('.apples').closest('.oranges').find('.nuts').addClass('border-solid');
+                $('.peaches').find('img').attr("alt", "I love fries");
+                $('img').parent().prev('.nuts').find('a').css({
+                    "background-color": "blue",
+                    "font-family": "Arial"
+                });
+                $('.nuts').find('p').html("I Love Avocados");
+
+            }
+        }
+    };
 
 
 })(jQuery, Drupal, this, this.document);
