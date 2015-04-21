1c8909a7c60193280851daed78006f205e6b34d6
Carl Watson <carl.watson@acquia.com>
2015-04-21 11:58:02 -0400

    added script.js

--- drupal/sites/all/themes/custom/acquiazen_theme/js/script.js
+++ drupal/sites/all/themes/custom/acquiazen_theme/js/script.js
@@ -10,10 +10,15 @@
       console.log('test');
 
 
+    $('.peaches').find('.nuts').addClass("border-yellow");
+    $('.apples').closest('.oranges').find('.nuts').addClass('border-solid');
+    $('.peaches').find('img').attr("alt","I love fries");
+    $('img').parent().prev('.nuts').find('a').css({"background-color":"blue", "font-family":"Arial"});
+    $('.nuts').find('p').html("I Love Avocados");
 
 
 
-
+    //$('.apples').parent('.oranges').add('.nuts').addClass('border-solid');
     }
   };
 
