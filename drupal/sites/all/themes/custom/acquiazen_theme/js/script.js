2acf360d3c638d5ac21a9a1e3f1f0e2b1aaf9e8d
Carl Watson <carl.watson@acquia.com>
2015-04-21 16:59:44 -0400

    finished jquery filter/each project

--- drupal/sites/all/themes/custom/acquiazen_theme/js/script.js
+++ drupal/sites/all/themes/custom/acquiazen_theme/js/script.js
@@ -1,84 +1,103 @@
 (function ($, Drupal, window, document, undefined) {
 
-  /**
-   * Example A
-   */
-  Drupal.behaviors.ExampleA = {
-    attach: function (context, settings) {
+    /**
+     * Example A
+     */
+    Drupal.behaviors.ExampleA = {
+        attach: function (context, settings) {
 
-      console.log('Example A loaded');
+            console.log('Example A loaded');
 
-    }
-  };
+        }
+    };
 
-  /**
-   * Hey Carl, we are using this for JS training ~
-   */
-  Drupal.behaviors.ExampleB = {
-    attach: function (context, settings) {
-      if (context === document) {
+    /**
+     * Hey Carl, we are using this for JS training ~
+     */
+    Drupal.behaviors.ExampleB = {
+        attach: function (context, settings) {
+            if (context === document) {
 
-        console.log('Example B is loaded');
-
-        
-
-        // using variable
-        var frenchfry = $('div.apples img');
-        // grab attr
-        var frenchfryAlt = frenchfry.attr('alt');
-        console.log(frenchfryAlt);
-
-        // https://api.jquery.com/category/traversing/
-        $('.oranges .peaches').find('span').addClass('MARKERS');
-        $('.apples').closest('.oranges').addClass('anotherClass');
-
-        var needText = $('.nuts p').html();
-        // console.log(needText);
-
-        $('.apples').after('<div class="Carl-Faux-Div"> </div>');
-        $('.Carl-Faux-Div').wrapInner('<div class="inside-this"></div>');
-        $('div.inside-this').html('<div class="whatever-next"></div><div class="whatever-prev"></div>');
-        $('.Carl-Faux-Div .whatever-next').html(needText);
-        $('.Carl-Faux-Div .whatever-prev').html('something else ');
+                console.log('Example B is loaded');
 
 
-        // each example
-        $(".banana ul li").each(function (index, element) {
-          var thisText = $(this).html();
-          // console.log(thisText);
-        });
+                // using variable
+                var frenchfry = $('div.apples img');
+                // grab attr
+                var frenchfryAlt = frenchfry.attr('alt');
+                console.log(frenchfryAlt);
+
+                // https://api.jquery.com/category/traversing/
+                $('.oranges .peaches').find('span').addClass('MARKERS');
+                $('.apples').closest('.oranges').addClass('anotherClass');
+
+                var needText = $('.nuts p').html();
+                // console.log(needText);
+
+                $('.apples').after('<div class="Carl-Faux-Div"> </div>');
+                $('.Carl-Faux-Div').wrapInner('<div class="inside-this"></div>');
+                $('div.inside-this').html('<div class="whatever-next"></div><div class="whatever-prev"></div>');
+                $('.Carl-Faux-Div .whatever-next').html(needText);
+                $('.Carl-Faux-Div .whatever-prev').html('something else ');
 
 
-        //Carl's code
-        /*
-        $('.peaches').find('.nuts').addClass("border-yellow");
-         $('.apples').closest('.oranges').find('.nuts').addClass('border-solid');
-         $('.peaches').find('img').attr("alt", "I love fries");
-         $('img').parent().prev('.nuts').find('a').css({
-         "background-color": "blue",
-         "font-family": "Arial"
-         });
-         $('.nuts').find('p').html("I Love Avocados");
-       */
+                // each example
+                $(".banana ul li").each(function (index, element) {
+                    var thisText = $(this).html();
+                    // console.log(thisText);
+                });
 
-        //#1
-        $('.nuts').after('<div class="Burma"></div>');
-          var mangosGrow = $('.MARKERS').html();
-          //console.log(mangosGrow);
-          $('.Burma').html(mangosGrow);
 
-          //#2
-          $(".peaches").wrapInner( "<div class='cream'></div>");
+                //Carl's code
+                /*
+                 $('.peaches').find('.nuts').addClass("border-yellow");
+                 $('.apples').closest('.oranges').find('.nuts').addClass('border-solid');
+                 $('.peaches').find('img').attr("alt", "I love fries");
+                 $('img').parent().prev('.nuts').find('a').css({
+                 "background-color": "blue",
+                 "font-family": "Arial"
+                 });
+                 $('.nuts').find('p').html("I Love Avocados");
+                 */
 
-          //#3
-          $(".apples").wrap("<div class='cheese'></div>");
+                //#1
+                $('.nuts').after('<div class="Burma"></div>');
+                var mangosGrow = $('.MARKERS').html();
+                //console.log(mangosGrow);
+                $('.Burma').html(mangosGrow);
 
-          //#4
-          
+                //#2
+                $(".peaches").wrapInner("<div class='banana'></div>");
 
-      }
-    }
-  };
+                //#3
+                $(".apples").wrap("<div class='cheese'></div>");
+
+                //Adding the Banana list
+                $('.banana').after("<ul><li>Rum</li><li>Vodka</li><li class='no'>Tequila</li><li>Whiskey</li></ul>");
+
+                //#4
+                $("li").each(function () {
+                    $(this).addClass("spirit");
+                });
+
+                //#5
+                $('li').not(".no").each(function () {
+                    $(this).addClass("EurAsia");
+                });
+
+                //#6
+                $('li').filter(".no").each(function () {
+                    $(this).addClass("Americas");
+                });
+
+                //#7
+                $('li').each(function () {
+                    $(this).wrapInner("<div class='drink'></div>");
+                });
+
+            }
+        }
+    };
 
 
 })(jQuery, Drupal, this, this.document);
